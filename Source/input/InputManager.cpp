3ad8520a67bc2eaf16d12533292c8abfb337f721
cchampet <cl.champetier@gmail.com>
2014-02-22 13:32:00 +0100

    Add movement with the mouse and keyboard

--- Source/input/InputManager.cpp
+++ Source/input/InputManager.cpp
@@ -10,53 +10,41 @@ namespace input{
 
 	}
 
-	bool InputManager::keyPressed(const KeyPress& keyPress, Component* ) //originatingComponent
-	{
+	bool InputManager::keyPressed(const KeyPress& keyPress, Component*)  {
 		int iKeyCode = toupper(keyPress.getKeyCode());
 
 		//ECHAP
-		if ( iKeyCode == KeyPress::escapeKey )
-		{
+		if ( iKeyCode == KeyPress::escapeKey ){
 			JUCEApplication::quit();
 			return true;
 		}
 		//UP
-		if ( iKeyCode == KeyPress::upKey )
-		{
-			//m_camera.RotateOrbit( 0, 0, LeapUtil::kfHalfPi * -0.05f );
-			//m_camera.Move(Leap::Vector(0.f, 0.2f, 0.f));
+		if ( iKeyCode == KeyPress::upKey ){
+			m_pRenderer->setTotalMotionTranslation(m_pRenderer->getTotalMotionTranslation() - Leap::Vector(0, 1, 0));
 			return true;
 		}
 		//DOWN
-		if ( iKeyCode == KeyPress::downKey )
-		{
-			//m_camera.RotateOrbit( 0, 0, LeapUtil::kfHalfPi * 0.05f );
-			//m_camera.Move(Leap::Vector(0.f, -0.2f, 0.f));
+		if ( iKeyCode == KeyPress::downKey ){
+			m_pRenderer->setTotalMotionTranslation(m_pRenderer->getTotalMotionTranslation() + Leap::Vector(0, 1, 0));
 			return true;
 		}
 		//LEFT
-		if ( iKeyCode == KeyPress::leftKey )
-		{
-			//m_camera.RotateOrbit( 0, LeapUtil::kfHalfPi * -0.05f, 0 );
-			//m_camera.Move(Leap::Vector(-0.2f, 0.f, 0.f));
+		if ( iKeyCode == KeyPress::leftKey ){
+			m_pRenderer->setTotalMotionTranslation(m_pRenderer->getTotalMotionTranslation() + Leap::Vector(1, 0, 0));
 			return true;
 		}
 		//RIGHT
-		if ( iKeyCode == KeyPress::rightKey )
-		{
-			//m_camera.RotateOrbit( 0, LeapUtil::kfHalfPi * 0.05f, 0 );
-			//m_camera.Move(Leap::Vector(0.2f, 0.f, 0.f));
+		if ( iKeyCode == KeyPress::rightKey ){
+			m_pRenderer->setTotalMotionTranslation(m_pRenderer->getTotalMotionTranslation() - Leap::Vector(1, 0, 0));
 			return true;
 		}
 		//ESCAPE
-		if ( iKeyCode == KeyPress::escapeKey )
-		{
+		if ( iKeyCode == KeyPress::escapeKey ){
 			m_pRenderer->isPaused(!m_pRenderer->isPaused());
 			return true;
 		}
 		//Caracters
-		switch( iKeyCode )
-		{
+		switch( iKeyCode ){
 			case ' ':
 				m_pRenderer->isPaused(!m_pRenderer->isPaused());
 				break;
@@ -73,25 +61,18 @@ namespace input{
 			default:
 				return false;
 		}
-
 		return true;
 	}
 
-	void InputManager::mouseDown (const MouseEvent& e)
-	{
-		//m_camera.OnMouseDown( LeapUtil::FromVector2( e.getPosition() ) );
+	void InputManager::mouseDown (const MouseEvent&){
 	}
 
-	void InputManager::mouseDrag (const MouseEvent& e)
-	{
-		//m_camera.OnMouseMoveOrbit( LeapUtil::FromVector2( e.getPosition() ) );
-		m_pRenderer->getOpenGLContext()->triggerRepaint();
+	void InputManager::mouseDrag (const MouseEvent&){
 	}
 
-	void InputManager::mouseWheelMove ( const MouseEvent& e, const MouseWheelDetails& wheel )
-	{
+	void InputManager::mouseWheelMove ( const MouseEvent& e, const MouseWheelDetails& wheel ){
 		(void)e;
-		//m_camera.OnMouseWheel( wheel.deltaY );
+		m_pRenderer->setTotalMotionTranslation(m_pRenderer->getTotalMotionTranslation() + Leap::Vector(0, 0, wheel.deltaY));
 		m_pRenderer->getOpenGLContext()->triggerRepaint();
 	}
 }
